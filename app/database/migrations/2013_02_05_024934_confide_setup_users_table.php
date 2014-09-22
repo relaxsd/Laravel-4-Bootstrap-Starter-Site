8ba576cf310ada7c85e80df79a20aaa48c16c971
MK <mk@relax-software.nl>
2014-09-23 01:47:09 +0200

    Re-generated migration with Confide 4

--- app/database/migrations/2013_02_05_024934_confide_setup_users_table.php
+++ app/database/migrations/2013_02_05_024934_confide_setup_users_table.php
@@ -1,22 +1,19 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 
-class ConfideSetupUsersTable extends Migration {
-
+class ConfideSetupUsersTable extends Migration
+{
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         // Creates the users table
-        Schema::create('users', function($table)
-        {
-            $table->engine = 'InnoDB';
+        Schema::create('users', function ($table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('email');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
             $table->string('password');
             $table->string('confirmation_code');
             $table->string('remember_token')->nullable();
@@ -24,11 +21,8 @@ class ConfideSetupUsersTable extends Migration {
             $table->timestamps();
         });
 
-
         // Creates password reminders table
-        Schema::create('password_reminders', function($table)
-        {
-            $table->engine = 'InnoDB';
+        Schema::create('password_reminders', function ($table) {
             $table->string('email');
             $table->string('token');
             $table->timestamp('created_at');
@@ -37,13 +31,10 @@ class ConfideSetupUsersTable extends Migration {
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
         Schema::drop('password_reminders');
         Schema::drop('users');
     }
-
 }
