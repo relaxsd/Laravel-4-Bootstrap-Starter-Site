5c8cd2df2ae4a167aab90ecc5c8de64fa276f949
MK <mk@relax-software.nl>
2014-09-23 15:27:44 +0200

    Removed isConfirmed tests (method was removed in Confide 4)

--- app/tests/models/UserTest.php
+++ app/tests/models/UserTest.php
@@ -11,30 +11,6 @@ class UserTest extends TestCase {
         $this->assertEquals( $user->username, 'admin' );
     }
 
-    public function testIsConfirmedByEmail()
-    {
-        $user = Woodling::retrieve('UserAdmin');
-        $this->assertEquals( $user->isConfirmed(array('email'=>'admin@example.org')), 1 );
-    }
-
-    public function testIsConfirmedByEmailFail()
-    {
-        $user = Woodling::retrieve('UserAdmin');
-        $this->assertNotEquals( $user->isConfirmed(array('email'=>'non-user@example.org')), true );
-    }
-
-    public function testIsConfirmedByUsername()
-    {
-        $user = Woodling::retrieve('UserAdmin');
-        $this->assertEquals( $user->isConfirmed(array('username'=>'admin')), true );
-    }
-
-    public function testIsConfirmedByUsernameFail()
-    {
-        $user = Woodling::retrieve('UserAdmin');
-        $this->assertNotEquals( $user->isConfirmed(array('username'=>'non-user')), true );
-    }
-
     public function testGetByUsername()
     {
         $user = Woodling::retrieve('UserAdmin');
